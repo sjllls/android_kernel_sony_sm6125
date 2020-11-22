b3e21ba8172035bb16b7178cf3cea7a75b961c5b
sjllls <2012ji123@gmail.com>
2020-11-22 11:55:41 +0800

    drivers: char: Import OSS changes

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -11,6 +11,11 @@
  * GNU General Public License for more details.
  *
  */
+/*
+ * NOTE: This file has been modified by Sony Mobile Communications Inc.
+ * Modifications are Copyright (c) 2020 Sony Mobile Communications Inc,
+ * and licensed under the license of the file.
+ */
 #include <linux/dma-buf.h>
 #include <linux/dma-mapping.h>
 #include <linux/slab.h>
